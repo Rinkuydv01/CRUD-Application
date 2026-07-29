e0502b9b91adfc9e2df8408c922905d357281943
Rinku Yadav <rinkuydv0102@gmail.com>
2026-07-30 00:22:45 +0530

    Return exception as DTO instead of String

--- src/main/java/com/example/StudentProject/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/StudentProject/exception/GlobalExceptionHandler.java
@@ -1,31 +1,60 @@
 package com.example.StudentProject.exception;
 
 
+import com.example.StudentProject.dto.ExceptionResponseDto;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler{
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> handleResourceNotFoundException(RuntimeException ex){
+    public ResponseEntity<ExceptionResponseDto> handleResourceNotFoundException(
+            RuntimeException ex, HttpServletRequest request){
+        ExceptionResponseDto exceptionResponse = new ExceptionResponseDto(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body(ex.getMessage());
+                .body(exceptionResponse);
     }
 
     @ExceptionHandler(DuplicateResourceException.class)
-    public ResponseEntity<String> handleDuplicateResourceException(RuntimeException ex){
+    public ResponseEntity<ExceptionResponseDto> handleDuplicateResourceException(
+            RuntimeException ex,HttpServletRequest request){
+        ExceptionResponseDto exceptionResponse = new ExceptionResponseDto(
+                LocalDateTime.now(),
+                HttpStatus.CONFLICT.value(),
+                HttpStatus.CONFLICT.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
-                .body(ex.getMessage());
+                .body(exceptionResponse);
     }
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleRuntimeException(RuntimeException ex){
+    public ResponseEntity<ExceptionResponseDto> handleRuntimeException(
+            RuntimeException ex,HttpServletRequest request){
+
+        ExceptionResponseDto exceptionResponse = new ExceptionResponseDto(
+                LocalDateTime.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("Something went wrong. Please try after some time");
+                .body(exceptionResponse);
     }
 }
